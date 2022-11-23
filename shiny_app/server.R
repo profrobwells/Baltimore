85764fe38cddfe2c419db03d8b8d39453aec765b
shreyavut <snvuttal@umd.edu>
2022-11-23 11:33:14 -0500

    shiny app + some changes

---
+++ shiny_app/server.R
@@ -0,0 +1,186 @@
+## load libraries
+library(tidyverse)
+library(tidycensus)
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(janitor)
+library(sf)
+library(lubridate)
+library(leaflet)
+library(leaflet.extras)
+library(scales)
+#install.packages('shinyjs')
+library(shinyjs)
+library(htmlwidgets)
+
+### zoom less on search
+### drop opacity on income map 
+### redlining legend
+### move around columns on table, neighborhood + income + population + demographics, census tract at end 
+## by neighborhood, income and population for visualizations
+
+
+race_table <- readRDS("data/balt_race_percentages_join.rds") %>%
+  filter(!is.na(percent_white))
+
+redlining <- readRDS("data/d_regions.rds") %>%
+  filter(gisjoin != "G2400050400800" &
+         gisjoin != "G2400050401505" &
+         gisjoin != "G2400050400900" &
+         gisjoin != "G2400050401504")
+
+percap_income_table <- readRDS("data/baltcity_percap_income.rds")
+
+race_and_income_table <- readRDS("data/race_and_income_combined.rds") 
+  
+
+## maybe session later
+server <- function(input, output, session){
+  
+  showLog()
+  logjs("App started")
+  
+    group_to_map <- reactive({
+      input$race
+    })
+    
+    output$mymap <- renderLeaflet({
+      
+      leaflet() %>%
+        addProviderTiles(providers$CartoDB.Positron) %>%
+        setView(lng=-76.6122, lat=39.2904, zoom=12) 
+      
+    })
+    
+    observeEvent(input$race, {
+      
+      mymap <- leafletProxy("mymap") %>%
+        clearControls()
+      
+      if (input$race == 'Change in Per Capita Income') {
+       # Color and palette are treated specially in the "superzip" case, because the values are categorical instead of continuous.
+        
+        shinyjs::logjs("hello")
+        
+        #getElement(group_to_map(), 'Change in Per Capita Income')
+      
+        pal <- colorQuantile("YlGnBu", domain=race_and_income_table[[group_to_map()]], reverse = TRUE, 10)
+        
+          leafletProxy("mymap") %>%
+          addPolygons(
+            data = race_and_income_table,
+            color = ~pal(race_and_income_table[[group_to_map()]]),
+            weight = 2.5,
+            smoothFactor = 0.2,
+            fillOpacity = 0.7,
+            ### add percent sign 
+            label = paste0(" Per capita income change is: ",(scales::dollar(race_and_income_table$'Change in Per Capita Income')))
+          ) %>%
+          clearControls %>%
+          addPolylines(
+              group = "Neighborhood",
+              data = percap_income_table,
+              color = "Gray",
+              weight = 1,
+              label = percap_income_table$community_statistical_area_2020,
+              fillOpacity = 0.75,
+              #options = layersControlOptions(collapsed = FALSE)
+            ) %>%
+          addPolylines(
+              group = "Redlining Zones",
+              data = redlining,
+              color = "Red",
+              weight = 5,
+              smoothFactor = 1
+            ) %>%
+          addLegend(
+            pal = pal,
+            position = "bottomleft",
+            values = race_and_income_table[[group_to_map()]],
+            title =  group_to_map() 
+          ) 
+        
+       } else {
+
+        
+      shinyjs::logjs("everything but income") 
+      
+      race_pal <- colorBin("YlGnBu", domain=race_and_income_table[[group_to_map()]], 5)
+       
+      
+      leafletProxy("mymap") %>%
+      clearShapes() %>%
+      clearControls() %>%
+      addPolygons(#group = "Black Population",
+          data = race_and_income_table,
+          color = ~race_pal(race_and_income_table[[group_to_map()]]),
+          weight = 2.5,
+          smoothFactor = 0.2,
+          fillOpacity = 0.7,
+          ### add percent sign 
+          label = paste0(input$race, " is", " ", race_and_income_table[[group_to_map()]],"%", " for", " ",race_and_income_table$Neighborhood, " |", " Per capita income change is: ",(scales::dollar(race_and_income_table$'Change in Per Capita Income')))
+        ) %>%
+        addPolylines(
+          group = "Neighborhood",
+          data = percap_income_table,
+          color = "Gray",
+          weight = 1,
+          label = percap_income_table$community_statistical_area_2020,
+          fillOpacity = 0.75,
+          options = layersControlOptions(collapsed = FALSE)
+        ) %>% 
+        addPolylines(
+          group = "Redlining Zones",
+          data = redlining,
+          color = "Red",
+          weight = 5,
+          smoothFactor = 1
+        ) %>%
+        #clearShapes() %>%
+        #clearControls() %>%
+      addLegend(
+        pal = race_pal,
+        position = "bottomleft",
+        values = race_and_income_table[[group_to_map()]],
+        title =  group_to_map()
+        ) %>%
+      addSearchFeatures(targetGroups = c("Neighborhood")
+                          ) 
+      
+       }
+      
+                 
+    })
+    
+    ## Data Explorer ###
+    
+    output$df <- DT::renderDataTable({
+      df <- race_and_income_table  %>%
+        st_drop_geometry(race_and_income_table) %>%
+        ## add 2020 per capita income
+        dplyr::select(-geoid, - name, -state, -census_tract, -object_id) %>%
+        rename(
+          'Total Population' = total_population,
+        )
+         # "Tract"= tract, "Neighborhood", "Percent White", "Percent Black", "Percent Hispanic/Latino", "Percent Asian", "Change in Per Capita Income") #%>%
+        # rename(
+        #   'Census Tract'= tract,
+        #   'Neighborhood' = neighborhood,
+        #   "Percent White" = percent_white,
+        #   "Percent Black" = percent_black,
+        #   "Percent Hispanic/Latino" = percent_hisp_lat,
+        #   "Percent Asian" = percent_asian,
+        #   "Change in Per Capita Income" = diff_2020_2010
+        # )
+
+      
+      action <- DT::dataTableAjax(session, df, outputId = "Data explorer")
+      DT::datatable(df, escape = FALSE)
+    
+    }) 
+}
+   
+
+
+
---
+++ shiny_app/ui.R
@@ -0,0 +1,89 @@
+library(tidyverse)
+library(tidycensus)
+library(shiny)
+library(shinythemes)
+library(shinyWidgets)
+library(janitor)
+library(lubridate)
+library(leaflet)
+library(leaflet.extras)
+
+
+## putting data here too?
+race_table <- readRDS("data/balt_race_percentages_join.rds")
+percap_income_table <- readRDS("data/baltcity_percap_income.rds")
+
+redlining <- readRDS("data/d_regions.rds") %>%
+  filter(gisjoin != "G2400050400800" &
+           gisjoin != "G2400050401505")
+
+race_and_income_table <- readRDS("data/race_and_income_combined.rds") 
+
+
+## defining vars for dropdowns
+# Choices for drop-downs
+vars <- c(
+    "Percent White" = "Percent White",
+    "Percent Black" = "Percent Black",
+    "Percent Hispanic/Latino" = "Percent Hispanic/Latino",
+    "Percent Asian" = "Percent Asian",
+    "Change in Per Capita Income" = "Change in Per Capita Income"
+)
+
+
+
+ui <- fluidPage(
+  
+  useShinyjs(),
+  
+  # App title ----
+  #titlePanel("Hello Shiny!"),
+  
+  # Sidebar layout with input and output definitions ----
+  navbarPage("Baltimore Reporting", id="nav",
+        
+    tabPanel("Interactive map",
+                      
+  # Main panel for displaying outputs ----
+
+    mainPanel(
+      div(class="outer",
+          
+      tags$head(
+        # Include our custom CSS
+        includeCSS("styles.css")
+      ),
+      
+      
+      leafletOutput("mymap", height="100%", width="100%" ),
+      
+      absolutePanel(id = "controls", class = "panel panel-default", fixed = FALSE,
+                    draggable = FALSE, top = 20, left = "auto", right = 20, bottom = "auto",
+                    width = 350, height = "auto",
+                    
+                    h4("Explore Income and Demographics"),
+                    
+                    #selectInput("race", "Race", vars)
+                    
+                    pickerInput(
+                      inputId = "race",
+                      choices = vars,
+                      options = list(
+                        `actions-box` = TRUE,
+                        size = 9,
+                        `selected-text-format` = "count > 3"
+                        
+                      )
+                    )
+      )
+      
+    ),
+        )
+  
+    ),
+  
+  tabPanel("Data explorer",
+           DT::dataTableOutput("df")
+  ),
+  )
+)
